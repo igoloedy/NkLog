7de86baf6b4402340231657681909e1ef59170cf
igoloedy <igoloedy@yahoo.co.jp>
2023-01-28 12:06:54 +0900

    add: ファイル出力と標準出力のログレベルを分ける機能追加

--- NkLog/NkLog.py
+++ NkLog/NkLog.py
@@ -10,7 +10,7 @@ class NkLog() :
     '''
     ログ管理クラス
     '''
-    def __init__(self,filename = '',format = '',level=10,stdout = False):
+    def __init__(self,filename = '',format = '',level=10,stdout = True,fileout=False):
         '''
         コンストラクタ
         
@@ -26,6 +26,9 @@ class NkLog() :
         stdout:bool
             ログを標準出力に表示するか否かの設定 
         '''
+        #標準出力及びファイル出力のログレベル
+        self._fh_loglevel = INFO
+        self._sd_loglevel = WARN
         
         #フォーマットが指定されていなければ、初期値を設定する。
         self.format = '[%(asctime)s] %(name)s (%(levelname)s) %(message)s' if format == '' else format
@@ -35,9 +38,9 @@ class NkLog() :
         #エラーレベルを設定
         self.logger.setLevel(level)
         #ハンドラを登録
-        self.set_handler(filename,self.format,stdout)
+        self.set_handler(filename,self.format,stdout=stdout,fileout=fileout)
     
-    def set_handler(self,filename,format,fileout = False, stdout = True):
+    def set_handler(self,filename,format,stdout = True,fileout = False):
         '''
         ハンドラの登録
         
@@ -52,21 +55,26 @@ class NkLog() :
         '''
         fmt = LoggingFormatter(format)    #フォーマッタの作成
         if fileout:
+            print(os.path.basename(__file__) + " - fileout:true")
             #ログファイル名が指定されていなければ、生成する
             self.filename = self.__create_log_path() if filename == '' else filename
             fh = LoggingFileHandler(filename) #ファイルハンドラの生成
             fh.setFormatter(fmt)               #ファイルハンドラにフォーマッタを登録
+            fh.setLevel(self._fh_loglevel)
             self.logger.addHandler(fh)         #ロガーにファイルハンドラを登録
         
         #標準出力の出力指示がされていたら、標準出力用のハンドラとフォーマッタを登録
         if stdout:
+            print(os.path.basename(__file__) + " - stdout:true")
             sd = LoggingStreamHandler(sys.stdout)
-            sd.setFormatter(fmt)     
+            sd.setFormatter(fmt)
+            sd.setLevel(self._sd_loglevel)
             self.logger.addHandler(sd)
     
     def setLevel(self,level):
         '''
         ログに出力するエラーレベルを設定する
+        note: これとは別にハンドラごとに個別にエラーレベルが設定されているので注意
         '''
         self.logger.setLevel(level)
     
@@ -128,8 +136,9 @@ class NkLog() :
 
 
 if __name__ == '__main__':
-    mg = NkLog(filename="log\\NkLogMainFunc.log", stdout=True)
-    mg.setLevel(WARN)
+    mg = NkLog(filename="log\\NkLogMainFunc.log", stdout=True, fileout=True)
+    mg.setLevel(DEBUG)
     mg.debug("debug")
+    mg.info("info")
     mg.warning("warn")
     mg.error("error")
