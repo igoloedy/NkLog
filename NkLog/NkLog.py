10744a967165173aa1540a298f031d07408e5cdc
igoloedy <igoloedy@yahoo.co.jp>
2023-01-28 12:40:16 +0900

    clear: lint

--- NkLog/NkLog.py
+++ NkLog/NkLog.py
@@ -2,18 +2,19 @@ from logging import getLogger, CRITICAL, FATAL, ERROR, WARN, INFO, DEBUG
 from logging import Formatter as LoggingFormatter
 from logging import FileHandler as LoggingFileHandler
 from logging import StreamHandler as LoggingStreamHandler
-from posixpath import dirname
-import sys
 import os
+import sys
 
-class NkLog() :
+
+class NkLog():
     '''
     ログ管理クラス
     '''
-    def __init__(self,filename = '',format = '',level=10,stdout = True,fileout=False):
+
+    def __init__(self, filename='', format='', level=10, stdout=True, fileout=False):
         '''
         コンストラクタ
-        
+
         Parameters
         ----------
         filename:str 
@@ -26,24 +27,24 @@ class NkLog() :
         stdout:bool
             ログを標準出力に表示するか否かの設定 
         '''
-        #標準出力及びファイル出力のログレベル
+        # 標準出力及びファイル出力のログレベル
         self._fh_loglevel = INFO
         self._sd_loglevel = WARN
-        
-        #フォーマットが指定されていなければ、初期値を設定する。
+
+        # フォーマットが指定されていなければ、初期値を設定する。
         self.format = '[%(asctime)s] %(name)s (%(levelname)s) %(message)s' if format == '' else format
-        
-        #ロガーを生成
+
+        # ロガーを生成
         self.logger = getLogger(os.path.basename(__file__))
-        #エラーレベルを設定
+        # エラーレベルを設定
         self.logger.setLevel(level)
-        #ハンドラを登録
-        self.set_handler(filename,self.format,stdout=stdout,fileout=fileout)
-    
-    def set_handler(self,filename,format,stdout = True,fileout = False):
+        # ハンドラを登録
+        self.set_handler(filename, self.format, stdout=stdout, fileout=fileout)
+
+    def set_handler(self, filename, format, stdout=True, fileout=False):
         '''
         ハンドラの登録
-        
+
         Parameters
         ----------
         filename:str 
@@ -53,82 +54,82 @@ class NkLog() :
         stdout:bool
             ログを標準出力に表示するか否かの設定 
         '''
-        fmt = LoggingFormatter(format)    #フォーマッタの作成
+        fmt = LoggingFormatter(format)  # フォーマッタの作成
         if fileout:
             print(os.path.basename(__file__) + " - fileout:true")
-            #ログファイル名が指定されていなければ、生成する
+            # ログファイル名が指定されていなければ、生成する
             self.filename = self.__create_log_path() if filename == '' else filename
-            fh = LoggingFileHandler(filename) #ファイルハンドラの生成
-            fh.setFormatter(fmt)               #ファイルハンドラにフォーマッタを登録
+            fh = LoggingFileHandler(filename)  # ファイルハンドラの生成
+            fh.setFormatter(fmt)  # ファイルハンドラにフォーマッタを登録
             fh.setLevel(self._fh_loglevel)
-            self.logger.addHandler(fh)         #ロガーにファイルハンドラを登録
-        
-        #標準出力の出力指示がされていたら、標準出力用のハンドラとフォーマッタを登録
+            self.logger.addHandler(fh)  # ロガーにファイルハンドラを登録
+
+        # 標準出力の出力指示がされていたら、標準出力用のハンドラとフォーマッタを登録
         if stdout:
             print(os.path.basename(__file__) + " - stdout:true")
             sd = LoggingStreamHandler(sys.stdout)
             sd.setFormatter(fmt)
             sd.setLevel(self._sd_loglevel)
             self.logger.addHandler(sd)
-    
-    def setLevel(self,level):
+
+    def setLevel(self, level):
         '''
         ログに出力するエラーレベルを設定する
         note: これとは別にハンドラごとに個別にエラーレベルが設定されているので注意
         '''
         self.logger.setLevel(level)
-    
-    def info(self,message):
+
+    def info(self, message):
         '''
         ログに情報メッセージを出力する
         '''
         self.logger.info(message)
-    
-    def error(self,message):
+
+    def error(self, message):
         '''
         ログにエラーメッセージを出力する
         '''
         self.logger.error(message)
-    
-    def exception(self,message):
+
+    def exception(self, message):
         '''
         ログに例外処理のメッセージを付加して出力する
         '''
         self.logger.exception(message)
-    
-    def warning(self,message):
+
+    def warning(self, message):
         '''
         ログに警告メッセージを出力する
         '''
         self.logger.warning(message)
-    
-    def debug(self,message):
+
+    def debug(self, message):
         '''
         ログにデバッグメッセージを出力する
         '''
         self.logger.debug(message)
 
-    def critical(self,message):
+    def critical(self, message):
         '''
         ログにクリティカルメッセージを出力する
         '''
         self.logger.critical(message)
-    
+
     def __create_log_path(self):
         '''
         __file__ の内容を使って、ログファイルのパスの初期値を生成する
         '''
-        folder = os.path.join(self.__get_parent(__file__),'logs')
+        folder = os.path.join(self.__get_parent(__file__), 'logs')
         name_without_ext = self.__get_name_without_ext(__file__)
-        return os.path.join(folder,name_without_ext+'.log')
-    
-    def __get_parent(self,path):
+        return os.path.join(folder, name_without_ext+'.log')
+
+    def __get_parent(self, path):
         '''
         指定したパスの１つ上のフォルダ（親フォルダ）を返す
         '''
-        return '/'.join(os.path.dirname(path).replace('\\','/').split('/')[0:-1])
-    
-    def __get_name_without_ext(self,filename):
+        return '/'.join(os.path.dirname(path).replace('\\', '/').split('/')[0:-1])
+
+    def __get_name_without_ext(self, filename):
         '''
         拡張子を除いたファイル名を返す
         '''
